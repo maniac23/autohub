a914a6d3e8921f6f39088b016b24f2045cbd3933
Mikhail Tukai <tukaym@gmail.com>
2016-05-22 17:12:31 +0300

    added cars rendering through ajax

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,13 @@
 'use strict';
 
 /* App Module */
-
-var autoHub = angular.module('autoHub', [ ]);
+angular.module('autoHub', [ ]);
 
 // Controller
-
 autoHub.controller('carsController', function ($scope, $http) {
-  $http.get('auto_data.json').success(function(data) {
-    $scope.cars = data;
-  });
-
+	$http.get('auto_data.json').success(function(data) {
+		$scope.cars = data;
+	});
 });
+
+
--- src/js/common.js
+++ src/js/common.js
@@ -39,3 +39,57 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// getting cars list from template
+var cars = [];
+var container = document.querySelector('.content');
+getCars();
+function renderCars(cars){
+  cars.forEach(function(car) {
+    var element = getElementFromTemplate(car);
+    container.appendChild(element);
+  });
+};
+
+
+function getElementFromTemplate(data) {
+  var template = document.querySelector('#car-template');
+  if ('content' in template) {
+    var element = template.content.children[0].cloneNode(true);
+  } else {
+    var element = template.children[0].cloneNode(true);
+  }
+  
+  element.querySelector('.item__name').textContent = data.name;
+  element.querySelector('.item__price').textContent = data.price + " $";
+  element.querySelector('.item__year').textContent = data.year;
+  element.querySelector('.item__distance').textContent = data.distance + " km";
+  element.querySelector('.item__gas').textContent = data.gas;
+  element.querySelector('.item__gear').textContent = data.gear;
+
+  var img = new Image();
+  img.onload = function(){
+    element.querySelector('.item__img').setAttribute("src", img.src );
+  }
+  img.src = data.url;
+
+  return element;
+};
+
+
+
+
+
+function getCars() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '../auto_data.json');
+  xhr.onload = function(e) {
+    var rawData = e.target.response;
+    var loadedCars = JSON.parse(rawData);
+
+    // drawing loaded data to DOM
+    renderCars(loadedCars);
+  }
+  xhr.send();
+
+};
